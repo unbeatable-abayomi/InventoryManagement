2e034294f4127e492f1c53b032291e621d41bac4
abayomi igwubor <igwubor@outlook.com>
2020-10-15 12:08:08 +0100

    addedAreaForItems

--- InventoryManagement/Startup.cs
+++ InventoryManagement/Startup.cs
@@ -62,7 +62,7 @@ namespace InventoryManagement
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+					pattern: "{area=Item}/{controller=Home}/{action=Index}/{id?}");
 				endpoints.MapRazorPages();
 			});
 		}
