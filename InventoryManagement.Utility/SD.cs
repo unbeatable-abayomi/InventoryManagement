00e6046663f3b33888f8b866d5385212a6d5668c
abayomi igwubor <igwubor@outlook.com>
2020-10-17 11:05:36 +0100

    addedDateCreatedToDisplayFields

--- InventoryManagement.Utility/SD.cs
+++ InventoryManagement.Utility/SD.cs
@@ -8,6 +8,10 @@ namespace InventoryManagement.Utility
 {
     public class SD
     {
-        
+        public const string Role_User_Indi = "Indiviual Customer";
+        public const string Role_User_Comp = "Company Customer";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+        public const string ssShoppingCart = "Shopping Cart Sessions";
     }
 }
